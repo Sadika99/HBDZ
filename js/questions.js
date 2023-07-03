76f080b095c87112fa2ee41b64fbeaf17dd44ec6
Sadika99 <94169048+Sadika99@users.noreply.github.com>
2023-07-04 00:03:03 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When did we first started texting?",
+    answer: "9 Oct 2023",
+    options: [
+      "8 Oct 2023",
+      "10 Oct 2023",
+      "9 Oct 2023",
+      "7 Oct 2023"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When was our First Audio Call?",
+    answer: "18 Oct 2023",
+    options: [
+      "18 Oct 2023",
+      "2 Nov 2023",
+      "24 Oct 2023",
+      "4 Nov 2023"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When was Your First Video call?",
+    answer: "19 Nov 2023",
+    options: [
+      "24 Nov 2023",
+      "19 Nov 2023",
+      "21 Nov 2023",
+      "14 Nov 2023"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is my Favorite nickname that you have given me",
+    answer: "Lolla 🐕",
+    options: [
+      "Lolla 🐕",
+      "Bulkee",
+      "Jilley",
+      "Kunjaee"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Where was I when I First Video Called you?",
+    answer: "At Mall",
+    options: [
+      "At Home",
+      "In a Bus",
+      "In a Car",
+      "At Mall"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
